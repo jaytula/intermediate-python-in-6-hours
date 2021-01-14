0baab1c725116da31db47c83e771d8b7873cc069
Jay T <jaytula@gmail.com>
2021-01-14 01:14:25 -0800

    Finish on tuples. In 42:41

--- 03-dictionaries.py
+++ 03-dictionaries.py
@@ -34,3 +34,53 @@ try:
   print(mydict["city"])
 except KeyError:
   print("Error")
+
+# Loop through dictionary
+mydict3 = dict(name="Lucas", age=28, city="Phoenix")
+for key in mydict3:
+  print(key, end=",")
+print("")
+
+for key in mydict3.keys():
+  print(key, end=",")
+print("")
+
+# Loop over values
+for value in mydict3.values():
+  print(value, end=",")
+print("")
+
+# Loop with both key and value available
+for key, value in mydict3.items():
+  print(key, value, end='|')
+print('')
+
+# Dictionaries are reference type. Be careful
+mydict3_cpy = mydict3
+mydict3_cpy["email"] = "max@xyz.com"
+
+print(mydict3_cpy)  
+print(mydict3)      # Also has email key
+
+# To do a true copy, use builtin copy method
+mydict3_real_copy = mydict3.copy()
+
+# Another way to copy is with dict
+mydict3_real_copy = dict(mydict3)
+
+# Merge two dictionaries with update
+mydict = dict(name="Max", age=28, email="max@xyz.com")
+mydict_2 = dict(name="Mary", age=27, city="boston")
+mydict.update(mydict_2)
+print(mydict)
+
+# Numbers can be keys
+my_dict = {3: 9, 6: 36, 9: 81}
+
+# Tuples can be keys
+mytuple = (8, 7)
+mydict = {mytuple: 15}
+print(mydict)
+
+# Note... A list cannot be a key
+# type must be hashable to be a key
