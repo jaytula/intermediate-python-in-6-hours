d4699efeabd8e434ff0f8ac7e3eba6d566f24bee
Jay T <jaytula@gmail.com>
2021-01-24 10:41:22 -0800

    Covered namedtuple, OrderDict, defaultdict. In 1:33:25

--- 06-collections.py
+++ 06-collections.py
@@ -1,5 +1,5 @@
 # collections: Counter, namedtuple, OrderedDict, defaultdict, deque
-from collections import Counter
+from collections import Counter, namedtuple, OrderedDict, defaultdict
 
 a = "aaaaabbbbccc"
 my_counter = Counter(a)
@@ -10,3 +10,22 @@ print(my_counter.values()) # dict_values([5, 4, 3])
 print(my_counter.most_common(2)) # [('a', 5), ('b', 4)]
 print(my_counter.elements()) # returns an iterable object: <itertools.chain object at 0x7efbfdfc4940>
 print(list(my_counter.elements())) # ['a', 'a', 'a', 'a', 'a', 'b', 'b', 'b', 'b', 'c', 'c', 'c']
+
+Point = namedtuple('Point', 'x,y')
+pt = Point(1, -4)
+print(pt)
+print(pt.x, pt.y)
+
+ordered_dict = OrderedDict()
+ordered_dict['a'] = 1
+ordered_dict['b'] = 2
+ordered_dict['c'] = 3
+ordered_dict['d'] = 4
+print(ordered_dict)
+
+# defaultdict: for undefined key, returns default value of type specified
+d = defaultdict(float)
+d['a'] = 1
+d['b'] = 2
+print(d)
+print(d['a'], d['b'], d['c'])
