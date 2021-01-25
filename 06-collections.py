d4912e88eb0ec008611a6ef0b098609aba541ef1
Jay T <jaytula@gmail.com>
2021-01-25 00:12:31 -0800

    deque. In 1:36:45

--- 06-collections.py
+++ 06-collections.py
@@ -1,5 +1,5 @@
 # collections: Counter, namedtuple, OrderedDict, defaultdict, deque
-from collections import Counter, namedtuple, OrderedDict, defaultdict
+from collections import Counter, namedtuple, OrderedDict, defaultdict, deque
 
 a = "aaaaabbbbccc"
 my_counter = Counter(a)
@@ -29,3 +29,33 @@ d['a'] = 1
 d['b'] = 2
 print(d)
 print(d['a'], d['b'], d['c'])
+
+# deque: double-ended queue
+d = deque()
+d.append(1)
+d.append(2)
+
+d.appendleft(3)
+print(d)
+
+d.pop()
+print(d)
+
+d.popleft()
+print(d)
+
+d.clear()
+print(d)
+
+d.extend([4,5,6])
+print(d)
+
+d.extendleft(['a', 'b', 'c'])
+print(d)
+
+d = deque([1, 2, 3, 4, 5])
+d.rotate(2)
+print(d) # [4, 5, 1, 2, 3]
+
+d.rotate(-1)
+print(d) # [5, 1, 2, 3, 4]
