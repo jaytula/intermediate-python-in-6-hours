ad0ad4fb1962c6d67c2db9afefb37cf610bea9f9
Jay T <jaytula@gmail.com>
2021-02-06 14:11:00 -0800

    JSON start. In 2:46:50

---
+++ 11-json.py
@@ -0,0 +1,11 @@
+import json
+
+person = {"name": "John", "age": 30, "city": "New York", "hasChildren": False, "titles": ["engineer", "programmer"]}
+
+print(json.dumps(person, indent=2))
+
+# Specify different separators using `separators`
+print(json.dumps(person, indent=2, separators=('; ', '= ')))
+
+# User sort_keys to sort keys
+print(json.dumps(person, indent=2, sort_keys=True))
