ef3b0cc70ca0d42b35de4a323444fe95f312fc09
Jay T <jaytula@gmail.com>
2021-02-07 01:32:29 -0800

    Encode custom object to JSON. In 2:54:06

--- 11-json.py
+++ 11-json.py
@@ -22,3 +22,20 @@ print(person)
 with open('person.json', 'r') as file:
   person = json.load(file)
   print(person)
+
+# Example of encoding custom object to JSON
+class User:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+user = User('Max', 27)
+
+def encode_user(o):
+  if isinstance(o, User):
+    return {'name': o.name, 'age': o.age, o.__class__.__name__: True}
+  else:
+    raise TypeError('Boject of type User is not JSON serializable')
+
+userJSON = json.dumps(user, default=encode_user)
+print(userJSON)
