6ae6658ff22361713960cda6d5940ceea5052973
Jay T <jaytula@gmail.com>
2021-02-08 10:06:55 -0800

    Two other ways to encode custom object. In 2:56:11

--- 11-json.py
+++ 11-json.py
@@ -39,3 +39,17 @@ def encode_user(o):
 
 userJSON = json.dumps(user, default=encode_user)
 print(userJSON)
+
+from json import JSONEncoder
+
+class UserEncoder(JSONEncoder):
+  def default(self, o):
+    if isinstance(o, User):
+      return {'name': o.name, 'age': o.age, o.__class__.__name__: True}
+    return JSONEncoder.default(self, o)
+
+userJSON = json.dumps(user, cls=UserEncoder)
+print(userJSON)
+
+userJSON = UserEncoder().encode(user)
+print(userJSON)
