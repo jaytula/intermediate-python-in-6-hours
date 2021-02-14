42e271a7a3418ae42730be892df952299d61741b
Jay T <jaytula@gmail.com>
2021-02-14 00:12:20 -0800

    Example decorator for function with arguments. In 3:21:01

--- 13-decorators.py
+++ 13-decorators.py
@@ -16,3 +16,18 @@ def print_name():
 
 #print_name = start_end_decorator(print_name) # What actually happens when decorating
 print_name()
+
+
+def start_end_decorator_2(func):
+  def wrapper(*args, **kwargs):
+    print("Start2")
+    result = func(*args, **kwargs)
+    print("End2")
+    return result
+  return wrapper
+
+@start_end_decorator_2
+def add5(x):
+  return x + 5
+
+print(add5(7))
