081186e64c9b0a043219154555a3d1d9beca3ebf
Jay T <jaytula@gmail.com>
2021-03-11 11:06:12 -0800

    Threading using a queue. In 4:28:39

--- 15-threading-queues.py
+++ 15-threading-queues.py
@@ -1,21 +1,29 @@
-from threading import Thread, Lock
+from threading import Thread, Lock, current_thread
 from queue import Queue
 import time
 
+def worker(q, lock):
+  while True:
+    value = q.get()
+
+    # processing..
+    with lock:
+      print(f'in {current_thread().name} got {value}')
+    q.task_done()
+
 if __name__ == "__main__":
   q = Queue()
+  lock = Lock()
 
-  q.put(1)
-  q.put(2)
-  q.put(3)
+  num_threads = 10
+  for i in range(num_threads):
+    thread = Thread(target=worker, args=(q, lock))
+    thread.daemon=True
+    thread.start()
 
-  # 3 2 1
-  first = q.get()
-  print(first) # 1
+  for i in range(1, 21):
+    q.put(i)
 
-  isEmpty = q.empty()
+  q.join()
 
-  q.task_done()
-
-  q.join() # blocks until all elements in queue are processed
   print('end main')
